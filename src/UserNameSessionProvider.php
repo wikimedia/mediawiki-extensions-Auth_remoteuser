c0451d5f8d7151a4f9f1cf1974361c324f5ded6f
Stefan Engelhardt <stefan.engelhardt@dlr.de>
2018-03-23 23:10:02 +0100

    Fully embrace associative arrays
    Array union operator `+=` did not work as intended because it was used on non
    associative arrays. Replaced it with real associative arrays.
    
    Bug: T188195
    Change-Id: If8f6797279d9f2f23f9c8029e75368a313531d7b

--- src/UserNameSessionProvider.php
+++ src/UserNameSessionProvider.php
@@ -468,13 +468,13 @@ class UserNameSessionProvider extends CookieSessionProvider {
 		# Disable any special pages related to user switching.
 		if ( !$this->switchUser ) {
 			$disableSpecialPages += [
-				'Userlogin',
-				'Userlogout',
-				'CreateAccount',
-				'LinkAccounts',
-				'UnlinkAccounts',
-				'ChangeCredentials',
-				'RemoveCredentials'
+				'Userlogin' => true,
+				'Userlogout' => true,
+				'CreateAccount' => true,
+				'LinkAccounts' => true,
+				'UnlinkAccounts' => true,
+				'ChangeCredentials' => true,
+				'RemoveCredentials' => true,
 			];
 		}
 
@@ -484,7 +484,10 @@ class UserNameSessionProvider extends CookieSessionProvider {
 
 		# Disable password related special pages and hide preference option.
 		if ( !$switchedUser ) {
-			$disableSpecialPages += [ 'ChangePassword', 'PasswordReset' ];
+			$disableSpecialPages += [
+				'ChangePassword' => true,
+				'PasswordReset' => true,
+			];
 			global $wgHiddenPrefs;
 			$wgHiddenPrefs[] = 'password';
 		}
@@ -556,7 +559,7 @@ class UserNameSessionProvider extends CookieSessionProvider {
 				);
 			}
 		} elseif ( !$switchedUser ) {
-			$disablePersonalUrls[] = 'logout';
+			$disablePersonalUrls += [ 'logout' => true ];
 		}
 
 		# Set user preferences on account creation only.
@@ -597,9 +600,9 @@ class UserNameSessionProvider extends CookieSessionProvider {
 			# Disable special pages related to email preferences.
 			if ( array_key_exists( 'email', $preferences ) ) {
 				$disableSpecialPages += [
-					'ChangeEmail',
-					'Confirmemail',
-					'Invalidateemail'
+					'ChangeEmail' => true,
+					'Confirmemail' => true,
+					'Invalidateemail' => true,
 				];
 			}
 
@@ -644,8 +647,8 @@ class UserNameSessionProvider extends CookieSessionProvider {
 		Hooks::register(
 			'SpecialPage_initList',
 			function ( &$specials ) use ( $disableSpecialPages ) {
-				foreach ( $disableSpecialPages as $page ) {
-					unset( $specials[ $page ] );
+				foreach ( $disableSpecialPages as $page => $true ) {
+					if ( $true ) { unset( $specials[ $page ] ); }
 				}
 				return true;
 			}
@@ -654,8 +657,8 @@ class UserNameSessionProvider extends CookieSessionProvider {
 		Hooks::register(
 			'PersonalUrls',
 			function ( &$personalurls ) use ( $disablePersonalUrls ) {
-				foreach ( $disablePersonalUrls as $url ) {
-					unset( $personalurls[ $url ] );
+				foreach ( $disablePersonalUrls as $url => $true ) {
+					if ( $true ) { unset( $personalurls[ $url ] ); }
 				}
 				return true;
 			}
